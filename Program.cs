dbc5bbeffaf364001a8db354fd99da2360e025fc
Junpyo Kim <jpkimkorea@gmail.com>
2022-03-21 00:45:53 -0500

    Nlog

--- Program.cs
+++ Program.cs
@@ -1,12 +1,21 @@
 ﻿using System;
+using NLog.Web;
+using System.IO;
 
 namespace TicketingSystem2
 {
     class Program
     {
+       private static NLog.Logger logger = NLogBuilder.ConfigureNLog(Directory.GetCurrentDirectory() + "\\nlog.config").GetCurrentClassLogger();
+
         static void Main(string[] args)
         {
+            logger.Info("Program started");
+           
             Console.WriteLine("Hello World!");
+
+            logger.Info("Program ended");
+
         }
     }
 }
